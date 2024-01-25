92038354999505cb36a1da3cf256d4ce4e3e4aef
wellysonlee <martinsgwellyson@gmail.com>
2024-01-25 10:31:33 -0300

    episodios e temporadas

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Serie extends Model
+class Episode extends Model
 {
     use HasFactory;
-    protected $fillable = ['nome'];
 
-    public function temporadas(){
-        return $this->hasMany(Season::class);
+    public function season(){
+        return $this->belongsTo(Season::class);
     }
 }
