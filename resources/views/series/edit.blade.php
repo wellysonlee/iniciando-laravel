fe77191a442a8cceca8828941a6e985f3a949814
wellysonlee <martinsgwellyson@gmail.com>
2024-01-25 10:03:46 -0300

    exibindo erros

--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -1,3 +1,3 @@
 <x-layout title="Editar Séries '{{$serie->nome}}'">
-    <x-series.form :action="route('series.update', $serie->id)" :nome="$serie->nome"/>
+    <x-series.form :action="route('series.update', $serie->id)" :nome="$serie->nome" :update="true"/>
 </x-layout>
